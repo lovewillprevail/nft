81df6a1b7839b073605ab6e9babc7b9abe70c57b
lovewillprevail <84600582+lovewillprevail@users.noreply.github.com>
2022-06-01 00:11:09 +0200

    Add files via upload

--- docs/jury/google.js
+++ docs/jury/google.js
@@ -1,3 +1,3 @@
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-L8GRNSRC4P"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'G-L8GRNSRC4P\'); </script>')
+document.write('')
 
 
